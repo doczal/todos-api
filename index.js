d1ed4e44f00d6a7687d63a12bdeffb7f6cd773ca
Rizal <doczal@gmail.com>
2018-10-01 22:20:41 +0800

    Defined the create route

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
-  res.send('hi there from express!');
+  res.send('hi there from root route!');
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(3000, function() {
   console.log('app is running on port 3000.');
